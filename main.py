1b7278349f1c5ab78897d603371bea9c8c927e39
Kot <159570063+Kot0101@users.noreply.github.com>
2026-06-20 22:48:36 +0300

    анус сатаны

--- main.py
+++ main.py
@@ -1,73 +1,70 @@
 import sys
+# Заставляем Pygame думать, что у нас нет экрана и звука (Dummy-режим рендеринга в память)
+import os
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+
 import pygame
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.uix.graphicsimage import GraphicsImage
+from kivy.uix.image import Image # Используем стандартный Image вместо косячного GraphicsImage
 from kivy.graphics.texture import Texture
 
-# Разрешение твоей игры
 WIDTH, HEIGHT = 1280, 720
 
-class PygameRenderer(GraphicsImage):
+class GameScreen(Image):
     def __init__(self, **kwargs):
-        super(PygameRenderer, self).__init__(**kwargs)
+        super(GameScreen, self).__init__(**kwargs)
         
-        # 1. Инициализируем Pygame внутри Kivy
+        # Инициализируем Pygame программно в памяти
         pygame.init()
+        self.pg_surface = pygame.Surface((WIDTH, HEIGHT))
         
-        # 2. Создаем виртуальный холст в памяти (вместо display.set_mode)
-        self.screen = pygame.Surface((WIDTH, HEIGHT))
-        
-        # 3. Твои переменные игры
+        # Логика игры
         self.cube_x = WIDTH // 2
         self.cube_y = HEIGHT // 2
-        self.size = 100
+        self.size = 120
         self.dragging = False
 
-        # Создаем текстуру Kivy для вывода картинки Pygame
+        # Создаем пустую текстуру Kivy
         self.kivy_texture = Texture.create(size=(WIDTH, HEIGHT), colorfmt='rgb')
         self.texture = self.kivy_texture
 
-        # Запускаем игровой цикл на 60 FPS через Kivy Clock
-        Clock.schedule_interval(self.update_game, 1.0 / 60.0)
+        # Игровой цикл на 60 FPS
+        Clock.schedule_interval(self.update_frame, 1.0 / 60.0)
 
-    def update_game(self, dt):
-        # ---------------------------------------------------------
-        # ТВОЙ ИГРОВОЙ КОД (Всё, что было после while running:)
-        # Заменяй screen на self.screen
-        # ---------------------------------------------------------
+    def update_frame(self, dt):
+        # Логика границ
         self.cube_x = max(self.size // 2, min(self.cube_x, WIDTH - self.size // 2))
         self.cube_y = max(self.size // 2, min(self.cube_y, HEIGHT - self.size // 2))
 
-        # Отрисовка
-        self.screen.fill((20, 20, 20))
+        # РИСУЕМ НАШ ЛЮБИМЫЙ КВАДРАТ ЧЕРЕЗ PYGAME
+        self.pg_surface.fill((20, 20, 20))
         pygame.draw.rect(
-            self.screen, 
+            self.pg_surface, 
             (255, 0, 0), 
             (self.cube_x - self.size // 2, self.cube_y - self.size // 2, self.size, self.size)
         )
-        # ---------------------------------------------------------
 
-        # 4. Обновляем картинку на экране: быстро перекидываем пиксели из Pygame холста в текстуру Kivy
-        # Переворачиваем Y координаты (flipped=True)
-        self.kivy_texture.blit_buffer(self.screen.get_buffer(), colorfmt='rgb', bufferfmt='ubyte', flipped=True)
-        self.canvas.ask_update()
+        # Вытаскиваем сырые пиксели из Pygame и отдаем их в Kivy-текстуру
+        raw_pixels = self.pg_surface.get_buffer().raw
+        self.kivy_texture.blit_buffer(raw_pixels, colorfmt='rgb', bufferfmt='ubyte', flipped=True)
+        
+        # Принудительно обновляем экран
+        self.texture_update()
 
-    # --- Считывание тачей через Kivy (переводим в координаты Pygame) ---
+    # Тачи через Kivy
     def on_touch_down(self, touch):
-        # Ограничиваем считывание тачей только на нашем Game view
         if not self.collide_point(*touch.pos): return False
-        x = int((touch.x / self.width) * WIDTH)
-        y = int(((self.height - touch.y) / self.height) * HEIGHT)
         self.dragging = True
-        self.cube_x, self.cube_y = x, y
+        self.cube_x = int((touch.x / self.width) * WIDTH)
+        self.cube_y = int(((self.height - touch.y) / self.height) * HEIGHT)
         return True
 
     def on_touch_move(self, touch):
         if self.dragging:
-            x = int((touch.x / self.width) * WIDTH)
-            y = int(((self.height - touch.y) / self.height) * HEIGHT)
-            self.cube_x, self.cube_y = x, y
+            self.cube_x = int((touch.x / self.width) * WIDTH)
+            self.cube_y = int(((self.height - touch.y) / self.height) * HEIGHT)
         return True
 
     def on_touch_up(self, touch):
@@ -76,8 +73,7 @@ class PygameRenderer(GraphicsImage):
 
 class KDATApp(App):
     def build(self):
-        # Сразу возвращаем наш рендерер, KivyActivity его подхватит
-        return PygameRenderer()
+        return GameScreen()
 
 if __name__ == '__main__':
     KDATApp().run()
